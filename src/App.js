9c505609fa8857d508b33556f83ccd1b2ea15e08
stormzy22 <aawweb23@gmail.com>
2021-07-09 16:49:41 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ const App = memo(() => {
   const vidProgressRef = useRef();
   const vidProgressBarRef = useRef();
   const muteRef = useRef();
+  const zoomRef = useRef();
+  const containerRef = useRef();
   const [mute, setMute] = useState(false);
+  /*
+Start Ref
+*/
 
+  /**
+End REF 
+**/
   useEffect(() => {
     (function () {
       const vidState = vidRef.current;
@@ -20,81 +28,127 @@ const App = memo(() => {
       } else {
         setMute(false);
       }
-      if (vidState.paused) {
-        console.log("paused");
-        setPlayPause(true);
-      } else {
-        console.log("play");
-        setPlayPause(false);
-      }
     })();
   });
 
   useEffect(() => {
+    //Obtaining Handlers
+    const video = vidRef.current;
+    const play_Pause_Btn = play_Pause_Ref.current;
+    const volume = volumeRef.current;
+    const volumeBar = volumeBarRef.current;
+    const vidProgress = vidProgressRef.current;
+    const vidProgressBar = vidProgressBarRef.current;
+    const volMute = muteRef.current;
+    const zoomBtn = zoomRef.current;
+
     (function () {
-      const video = vidRef.current;
-      const play_Pause_Btn = play_Pause_Ref.current;
-      const volume = volumeRef.current;
-      const volumeBar = volumeBarRef.current;
-      const vidProgress = vidProgressRef.current;
-      const vidProgressBar = vidProgressBarRef.current;
-      const volMute = muteRef.current;
-
-      function updateProgress(e) {
-        const { duration, currentTime } = e.srcElement;
-        const progressPercent = (currentTime / duration) * 100;
-        vidProgressBar.style.width = `${progressPercent}%`;
-        if (video.ended) {
-          setPlayPause(true);
+      // Check if the browser actually support the video element?
+      const supportsVideo = !!document.createElement("video").canPlayType;
+      if (supportsVideo) {
+        function updateProgress(e) {
+          const { duration, currentTime } = e.srcElement;
+          const progressPercent = (currentTime / duration) * 100;
+          vidProgressBar.style.width = `${progressPercent}%`;
+          if (video.ended) {
+            setPlayPause(true);
+          }
         }
-      }
-      function setProgress(e) {
-        const width = this.clientWidth;
-        // console.log(width);
-        const clickX = e.offsetX;
-        // console.log(clickX);
-        const duration = video.duration;
+        function setProgress(e) {
+          const width = this.clientWidth;
+          // console.log(width);
+          const clickX = e.offsetX;
+          // console.log(clickX);
+          const duration = video.duration;
 
-        video.currentTime = (clickX / width) * duration;
-      }
-      function alterVolume(e) {
-        const width = this.clientWidth;
-        const clickX = e.offsetX;
-        video.volume = Number((clickX / width).toFixed(2));
-      }
-      function alterVidVolumeBar(e) {
-        const videoVolume = Math.round(video.volume * 100);
-        volumeBar.style.width = `${videoVolume}%`;
-        // console.log(videoVolume);
-      }
+          video.currentTime = (clickX / width) * duration;
+        }
+        function alterVolume(e) {
+          const width = this.clientWidth;
+          const clickX = e.offsetX;
+          video.volume = Number((clickX / width).toFixed(2));
+        }
+        function alterVidVolumeBar(e) {
+          const videoVolume = Math.round(video.volume * 100);
+          volumeBar.style.width = `${videoVolume}%`;
+          // console.log(videoVolume);
+        }
+        function playAndPause() {
+          if (video.paused || video.ended) {
+            setPlayPause(false);
+            video.play();
+          } else {
+            setPlayPause(true);
+            video.pause();
+          }
+        }
 
-      // handle Volume
-      volume.addEventListener("click", alterVolume);
-      volume.addEventListener("click", alterVidVolumeBar);
-      window.addEventListener("load", alterVidVolumeBar);
-      //handle the volume
-      volMute.addEventListener("click", () => {
-        video.muted = !video.muted;
-        if (video.muted) {
-          volumeBar.style.display = "none";
-        } else {
-          volumeBar.style.display = "block";
-        }
-        setMute(video.muted);
-      });
-      //toggle play and pause state
-      play_Pause_Btn.addEventListener("click", () => {
-        if (video.paused || video.ended) {
-          setPlayPause(false);
-          video.play();
-        } else {
-          setPlayPause(true);
-          video.pause();
-        }
-      });
-      //update video progress
-      video.addEventListener("timeupdate", updateProgress);
-      vidProgress.addEventListener("click", setProgress);
+        //------------Full screen--------------
+        // Checks if the document is currently in fullscreen mode
+        const isFullScreen = function () {
+          return !!(
+            document.fullScreen ||
+            document.webkitIsFullScreen ||
+            document.mozFullScreen ||
+            document.msFullscreenElement ||
+            document.fullscreenElement
+          );
+        };
+        // Fullscreen
+        const handleFullscreen = function () {
+          // If fullscreen mode is active...
+          if (isFullScreen()) {
+            // ...exit fullscreen mode
+            // (Note: this can only be called on document)
+            if (document.exitFullscreen) document.exitFullscreen();
+            else if (document.mozCancelFullScreen)
+              document.mozCancelFullScreen();
+            else if (document.webkitCancelFullScreen)
+              document.webkitCancelFullScreen();
+            else if (document.msExitFullscreen) document.msExitFullscreen();
+            // setFullscreenData(false);
+          } else {
+            // ...otherwise enter fullscreen mode
+            // (Note: can be called on document, but here the specific element is used as it will also ensure that the element's children, e.g. the custom controls, go fullscreen also)
+            if (video.requestFullscreen) video.requestFullscreen();
+            else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
+            else if (video.webkitRequestFullScreen) {
+              // Safari 5.1 only allows proper fullscreen on the video element. This also works fine on other WebKit browsers as the following CSS (set in styles.css) hides the default controls that appear again, and
+              // ensures that our custom controls are visible:
+              // figure[data-fullscreen=true] video::-webkit-media-controls { display:none !important; }
+              // figure[data-fullscreen=true] .controls { z-index:2147483647; }
+              video.webkitRequestFullScreen();
+            } else if (video.msRequestFullscreen) video.msRequestFullscreen();
+            // setFullscreenData(true);
+          }
+        };
+
+        // -----------------------------------------
+        // handle Volume
+        volume.addEventListener("click", alterVolume);
+        volume.addEventListener("click", alterVidVolumeBar);
+        window.addEventListener("load", alterVidVolumeBar);
+        //handle the volume
+        volMute.addEventListener("click", () => {
+          video.muted = !video.muted;
+          if (video.muted) {
+            volumeBar.style.display = "none";
+          } else {
+            volumeBar.style.display = "block";
+          }
+          setMute(video.muted);
+        });
+        //toggle play and pause state
+        play_Pause_Btn.addEventListener("click", playAndPause);
+        //update video progress
+        video.addEventListener("timeupdate", updateProgress);
+        vidProgress.addEventListener("click", setProgress);
+        //set keyBoard control
+        navigator.mediaSession.setActionHandler("play", playAndPause);
+        navigator.mediaSession.setActionHandler("pause", playAndPause);
+        //Handle full screen
+        zoomBtn.addEventListener("click", handleFullscreen);
+      }
     })();
   }, []);
 
@@ -102,9 +156,13 @@ const App = memo(() => {
     <div className="App bg-dark">
       <div className="h100 d-flex align-items-center">
         <div className="container main-container">
-          <figure className="bg-light">
-            <video id="vid" className="w-100" ref={vidRef}>
-              <source src="video/sintel-short.mp4" />
+          <figure
+            className="bg-light"
+            data-fullscreen="false"
+            ref={containerRef}
+          >
+            <video id="vid" className="w-100" controls={false} ref={vidRef}>
+              <source src="video/tears-of-steel-battle-clip-medium.mp4" />
             </video>
             <figcaption>
               <div className="progress-container p-2">
@@ -168,7 +226,7 @@ const App = memo(() => {
                 {/*-------------------- Seperate controls ----------- */}
                 <div className="minor-controls">
                   <div className="controls d-flex float-end">
-                    <button className="btn">
+                    <button className="btn" ref={zoomRef}>
                       <i className="fas fa-expand"></i>
                     </button>
                   </div>
